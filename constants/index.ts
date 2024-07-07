2d155381b5a4f9b0dc03ddc0bb64774d77db4f2d
PratyakshG <pincodeazm@gmail.com>
2024-07-07 13:59:33 +0530

    initial commit

---
+++ constants/index.ts
@@ -0,0 +1,35 @@
+export const sidebarLinks = [
+  {
+    igmUrl: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    igmUrl: "/icons/upcoming.svg",
+    route: "/upcoming",
+    label: "Upcoming",
+  },
+  {
+    igmUrl: "/icons/previous.svg",
+    route: "/previous",
+    label: "Previous",
+  },
+  {
+    igmUrl: "/icons/recordings.svg",
+    route: "/recordings",
+    label: "Recordings",
+  },
+  {
+    igmUrl: "/icons/add-personal.svg",
+    route: "/personal-room",
+    label: "Personal Room",
+  },
+];
+
+export const avatarImages = [
+  "/images/avatar-1.jpeg",
+  "/images/avatar-2.jpeg",
+  "/images/avatar-3.png",
+  "/images/avatar-4.png",
+  "/images/avatar-5.png",
+];
